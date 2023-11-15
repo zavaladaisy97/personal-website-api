58463b4d0a8d9835797b07f0e9d6986936f10fbe
daisyalexandria <dazavala7@gmail.com>
2023-11-15 13:20:44 -0600

    updated some controllers to fetch data from the db and render views

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,4 +1,5 @@
 class EducationsController < ApplicationController
   def index
+    @educations = Education.all
   end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,5 @@
 class SkillsController < ApplicationController
   def index
+    @skills = Skill.all
   end
 end
