02185c2cad7fc149aeff9a60a7ef98ae7060d014
daisyalexandria <dazavala7@gmail.com>
2023-11-15 12:24:47 -0600

    added projects, skills, educations, and experiences controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'experiences/index'
+  get 'educations/index'
+  get 'skills/index'
+  get 'projects/index'
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
